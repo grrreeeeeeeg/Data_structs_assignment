57c052fb74dc29b4743c23a9a27b41a6dd8b1e07
grrreeeeeeeg <152520400+grrreeeeeeeg@users.noreply.github.com>
2024-12-03 22:21:15 +0200

    Update DoubleQueueImpl.java

--- DoubleQueueImpl.java
+++ DoubleQueueImpl.java
@@ -1,29 +1,8 @@
-package edu.aueb.ds.lab5.SimpleQueue;
+
 
 import java.util.NoSuchElementException; 
 
-/**
- * Implementation of Queue using Array structure
- *
- */
 public class DoubleQueueImpl implements DoubleQueue {
-    /**
-     * In order to implement a Queue using Arrays we will use Array Rotation and dynamic array grow
-     * <p>
-     * Rotating an Array simply means shifting elements of array to left or
-     * to right by n position without exhausting the β€�bound of arrayβ€�.
-     * <p>
-     * To use array rotation we implement positionAfter(int current) function to get the
-     * next position after current. (e.x. if current is the last place of the array then
-     * next is the first place of the array - rotating around array)
-     * <p>
-     * When the array is full and we need to enqueue more items then we must grow the array.
-     * To do so we will use growQueue() function to increase the capacity of existing array
-     * by AUTOGROW_SIZE.
-     * <p>
-     * When array grows we need to pay attention to where we replace items, so we won't lose
-     * the order, and replace "front" and "back" pointers, as necessary.
-     */
 
     private double[] queueContents; // the array to hold the queue data
 
@@ -35,9 +14,6 @@ public class DoubleQueueImpl implements DoubleQueue {
     private static final int DEFAULT_CAPACITY = 2; // the default queue capacity
     private static final int AUTOGROW_SIZE = 2; // the autogrow size after reaching full capacity
 
-    /**
-     * Initialize the queue
-     */
     public DoubleQueueImpl() {
         queueContents = new double[DEFAULT_CAPACITY];
         front = 0;
@@ -45,12 +21,6 @@ public class DoubleQueueImpl implements DoubleQueue {
         size = 0;
     }
 
-    /**
-     * Insert a new item into the queue.
-     *
-     * @param item the item to insert.
-     */
-    @Override
     public void put(double item) {
         if (size == queueContents.length)
             growQueue();
@@ -61,14 +31,6 @@ public class DoubleQueueImpl implements DoubleQueue {
         size += 1;
     }
 
-    /**
-     * Return and remove the least recently inserted item
-     * from the queue.
-     *
-     * @return the least recently inserted item in the queue.
-     * @throws EmptyQueueException if the queue is empty.
-     */
-    @Override
     public double get() throws NoSuchElementException {
         if (isEmpty())
             throw new NoSuchElementException();
@@ -80,22 +42,10 @@ public class DoubleQueueImpl implements DoubleQueue {
         return element;
     }
 
-    /**
-     * Test if the queue is logically empty.
-     *
-     * @return true if empty, false otherwise.
-     */
-    @Override
     public boolean isEmpty() {
         return size == 0;
     }
 
-    /**
-     * Returns the next position in queue, after "current"
-     *
-     * @param current The current position
-     * @return the position in queue after current
-     */
     private int positionAfter(int current) {
         return (current + 1) % queueContents.length;
 
@@ -105,9 +55,6 @@ public class DoubleQueueImpl implements DoubleQueue {
         // return current + 1;
     }
 
-    /**
-     * Increases the maximum capacity of the queue base on AUTOGROW_SIZE
-     */
     private void growQueue() {
         double[] newContents = new double[queueContents.length + AUTOGROW_SIZE];
 
@@ -132,7 +79,7 @@ public class DoubleQueueImpl implements DoubleQueue {
        stream.print("Queue: [");
        if (!isEmpty()){
         int i = front;
-        while (i != back) {
+        while (i != positionAfter(back)) {
             stream.print(queueContents[i]+", ");
             i = positionAfter(i);
         }
