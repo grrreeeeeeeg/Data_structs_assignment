1d78394a1d7663e8880e925184836ce4a71e101c
grrreeeeeeeg <152520400+grrreeeeeeeg@users.noreply.github.com>
2024-12-04 11:32:46 +0200

    Update DoubleQueueImpl.java

--- DoubleQueueImpl.java
+++ DoubleQueueImpl.java
@@ -1,7 +1,12 @@
 
 
+import java.io.PrintStream;
 import java.util.NoSuchElementException; 
 
+/**
+ * Implementation of Queue using Array structure
+ *
+ */
 public class DoubleQueueImpl implements DoubleQueue {
 
     private double[] queueContents; // the array to hold the queue data
@@ -14,6 +19,9 @@ public class DoubleQueueImpl implements DoubleQueue {
     private static final int DEFAULT_CAPACITY = 2; // the default queue capacity
     private static final int AUTOGROW_SIZE = 2; // the autogrow size after reaching full capacity
 
+    /**
+     * Initialize the queue
+     */
     public DoubleQueueImpl() {
         queueContents = new double[DEFAULT_CAPACITY];
         front = 0;
@@ -21,6 +29,12 @@ public class DoubleQueueImpl implements DoubleQueue {
         size = 0;
     }
 
+    /**
+     * Insert a new item into the queue.
+     *
+     * @param item the item to insert.
+     */
+    @Override
     public void put(double item) {
         if (size == queueContents.length)
             growQueue();
@@ -31,6 +45,14 @@ public class DoubleQueueImpl implements DoubleQueue {
         size += 1;
     }
 
+    /**
+     * Return and remove the least recently inserted item
+     * from the queue.
+     *
+     * @return the least recently inserted item in the queue.
+     * @throws NoSuchElementException if the queue is empty.
+     */
+    @Override
     public double get() throws NoSuchElementException {
         if (isEmpty())
             throw new NoSuchElementException();
@@ -42,18 +64,27 @@ public class DoubleQueueImpl implements DoubleQueue {
         return element;
     }
 
+    /**
+     * Test if the queue is logically empty.
+     *
+     * @return true if empty, false otherwise.
+     */
+    @Override
     public boolean isEmpty() {
         return size == 0;
     }
 
+    /**
+     * Returns the next position in queue, after "current"
+     *
+     * @param current The current position
+     * @return the position in queue after current
+     */
     private int positionAfter(int current) {
         return (current + 1) % queueContents.length;
 
-        // Alternative
-        // if(current + 1 == queueContents.length)
-        //     return 0;
-        // return current + 1;
     }
+
 
     private void growQueue() {
         double[] newContents = new double[queueContents.length + AUTOGROW_SIZE];
@@ -69,29 +100,32 @@ public class DoubleQueueImpl implements DoubleQueue {
         back = size - 1;
     }
 
+	/**
+ 	 * return the oldest item of the queue
+ 	 * @return oldest item of the queue
+	 * @throws NoSuchElementException if the queue is empty
+	 */
     public double peek() throws NoSuchElementException {
         if (isEmpty())
             throw new NoSuchElementException();
         return queueContents[front];
     }
 
-    public void printQueue(PrintStream stream) {
-       stream.print("Queue: [");
-       if (!isEmpty()){
+	public void printQueue(PrintStream stream) {
+        stream.print("Queue: [ ");
         int i = front;
         while (i != positionAfter(back)) {
-            stream.print(queueContents[i]+", ");
-            i = positionAfter(i);
+            stream.print(queueContents[i] + ", ");
+            i = positionAfter[i];
         }
-       }
-       stream.print("]");
-
-         
+        stream.print("]");
     }
-    public int size() {
-        if (isEmpty()) {
-            return 0;
-        }
+
+	/**
+	 * return the size of the queue, 0 if it is empty
+	 * @return number of elements in the queue
+	 */
+	public int size() {
         return size;
     }
 }
